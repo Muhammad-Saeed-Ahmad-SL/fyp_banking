aa9badb6848cf91570c5378248778b4e1e545baa
Muhammad-Saeed-Ahmad-SL <saeed.ahmad@solutionsloft.com>
2024-06-01 23:44:34 +0500

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,42 @@
- import React from 'react'
- 
- const Home = () => {
-   return (
-     <div>page</div>
-   )
- }
- 
- export default Home 
+import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
+
+const Home = () => {
+  const loggedIn = {
+    firstName: "Saeed",
+    lastName: "Ahmad",
+    email: "contact@horizon.pro",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || "Guest"}
+            subtext={
+              "Access and manage your account and transactions efficiently."
+            }
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.3}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, {currentBalance: 500.50}]}
+      />
+    </section>
+  );
+};
+
+export default Home;
